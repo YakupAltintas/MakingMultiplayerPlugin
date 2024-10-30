815be9fcd73baa0866da825e070b615a2a7bbd29
Yakup Altintas <yakupgoldstone@gmail.com>
2024-10-30 14:06:42 +0300

    menu system created

---
+++ Plugins/MultiplayerSession/Source/MultiplayerSession/Private/Menu.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Menu.h"
+
+void UMenu::menuSetup()
+{
+	AddToViewport();
+	SetVisibility(ESlateVisibility::Visible);
+	bIsFocusable = true;
+
+	UWorld* world = GetWorld();
+	if (world)
+	{
+		APlayerController* playerController = world->GetFirstPlayerController();
+		if (playerController)
+		{
+			FInputModeUIOnly inputModeData;
+			inputModeData.SetWidgetToFocus(TakeWidget());
+			inputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+			playerController->SetInputMode(inputModeData);
+			playerController->SetShowMouseCursor(true);
+
+		}
+	}
+}
