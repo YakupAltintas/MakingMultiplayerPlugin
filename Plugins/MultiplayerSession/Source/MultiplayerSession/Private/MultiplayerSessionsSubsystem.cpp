98fb77031878115a9dda92afea08869e750190c2
Yakup Altintas <yakupgoldstone@gmail.com>
2024-10-30 13:29:35 +0300

    session functions and delegate veravles  created

--- Plugins/MultiplayerSession/Source/MultiplayerSession/Private/MultiplayerSessionsSubsystem.cpp
+++ Plugins/MultiplayerSession/Source/MultiplayerSession/Private/MultiplayerSessionsSubsystem.cpp
@@ -4,7 +4,12 @@
 #include "MultiplayerSessionsSubsystem.h"
 #include <OnlineSubsystem.h>
 
-UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem()
+UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem():
+createSessionComplateDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(this,&ThisClass::onCreateSessionComplate)),
+findSessionsComplateDelegate(FOnFindSessionsCompleteDelegate::CreateUObject(this,&ThisClass::onfindSessionsComplate)),
+joinSessionComplateDelegate(FOnJoinSessionCompleteDelegate::CreateUObject(this,&ThisClass::onJoinSessionComplate)),
+destroySessionComplateDelegate(FOnDestroySessionCompleteDelegate::CreateUObject(this,&ThisClass::onDestroySessionComplate)),
+startSessionComplateDelegate(FOnStartSessionCompleteDelegate::CreateUObject(this,&ThisClass::onStartSessionComplate))
 {
 	//OnlineSuubsystem pointer'ý oluþturduk
 	IOnlineSubsystem* subsystem = IOnlineSubsystem::Get();
@@ -13,4 +18,44 @@ UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem()
 		//oluþturduðumuz pointer'ý sessionInterface'imize atadýk
 		sessionInterface = subsystem->GetSessionInterface();
 	}
+}
+
+void UMultiplayerSessionsSubsystem::createSession(int32 NumpublicConnections, FString MatchType)
+{
+}
+
+void UMultiplayerSessionsSubsystem::findSession(int32 MaxSearchResult)
+{
+}
+
+void UMultiplayerSessionsSubsystem::joinSession(const FOnlineSessionSearchResult& SessionResult)
+{
+}
+
+void UMultiplayerSessionsSubsystem::DestroySession()
+{
+}
+
+void UMultiplayerSessionsSubsystem::StartSession()
+{
+}
+
+void UMultiplayerSessionsSubsystem::onCreateSessionComplate(FName SessionName, bool bWasSuccesful)
+{
+}
+
+void UMultiplayerSessionsSubsystem::onfindSessionsComplate(bool bWasSuccesful)
+{
+}
+
+void UMultiplayerSessionsSubsystem::onJoinSessionComplate(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+}
+
+void UMultiplayerSessionsSubsystem::onDestroySessionComplate(FName SessionName, bool bWasSuccesful)
+{
+}
+
+void UMultiplayerSessionsSubsystem::onStartSessionComplate(FName SessionName, bool bWasSuccesful)
+{
 }
--- Plugins/MultiplayerSession/Source/MultiplayerSession/Public/MultiplayerSessionsSubsystem.h
+++ Plugins/MultiplayerSession/Source/MultiplayerSession/Public/MultiplayerSessionsSubsystem.h
@@ -16,7 +16,36 @@ class MULTIPLAYERSESSION_API UMultiplayerSessionsSubsystem : public UGameInstanc
 	GENERATED_BODY()
 public:
 	UMultiplayerSessionsSubsystem();//yapýcý methodun oluþturduk
+
+	void createSession(int32 NumpublicConnections, FString MatchType);
+	void findSession(int32 MaxSearchResult);
+	void joinSession(const FOnlineSessionSearchResult &SessionResult);
+	void DestroySession();
+	void StartSession();
 protected:
+
+	void onCreateSessionComplate(FName SessionName,bool bWasSuccesful);
+	void onfindSessionsComplate(bool bWasSuccesful);
+	void onJoinSessionComplate(FName SessionName,EOnJoinSessionCompleteResult::Type Result);
+	void onDestroySessionComplate(FName SessionName, bool bWasSuccesful);
+	void onStartSessionComplate(FName SessionName, bool bWasSuccesful);
+
+
 private:
 	IOnlineSessionPtr sessionInterface;
+	FOnCreateSessionCompleteDelegate createSessionComplateDelegate;
+	FDelegateHandle createSessionComplateDelegateHandle;
+
+	FOnFindSessionsCompleteDelegate findSessionsComplateDelegate;
+	FDelegateHandle findSessionsComplateDelegateHandle;
+
+	FOnJoinSessionCompleteDelegate joinSessionComplateDelegate;
+	FDelegateHandle joinSessionComplateDelegateHandle;
+
+	FOnDestroySessionCompleteDelegate destroySessionComplateDelegate;
+	FDelegateHandle destroySessionComplateDelegateHandle;
+
+	FOnStartSessionCompleteDelegate startSessionComplateDelegate;
+	FDelegateHandle startSessionComplateDelegateHandle;
+
 };
